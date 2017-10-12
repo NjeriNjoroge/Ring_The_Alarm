c63a9937d2bcf2e2a8ffbf0c00cc5815389247a8
NjeriNjoroge <wanjorogegrace@gmail.com>
2017-10-12 11:48:54 +0300

    created an activity to display card views of private ambulances

--- app/src/main/java/com/example/gnjoroge/ringthealarm/AmbulanceActivity.java
+++ app/src/main/java/com/example/gnjoroge/ringthealarm/AmbulanceActivity.java
@@ -1,30 +1,37 @@
 package com.example.gnjoroge.ringthealarm;
 
 import android.content.Intent;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import butterknife.Bind;
-import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
 
-    @Bind(R.id.startAmb) Button mAmbulance;
+public class AmbulanceActivity extends AppCompatActivity {
+
+    @Bind(R.id.selectAmb) Button mSelectAmb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
+        setContentView(R.layout.activity_ambulance);
 
-        mAmbulance.setOnClickListener(new View.OnClickListener() {
+        mSelectAmb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, AmbulanceActivity.class);
+
+                Intent intent = new Intent(AmbulanceActivity.this, PrivateAmbList.class);
                 startActivity(intent);
             }
         });
+
+
+
     }
+
 }
+
---
+++ app/src/main/java/com/example/gnjoroge/ringthealarm/PrivateAmbList.java
@@ -0,0 +1,13 @@
+package com.example.gnjoroge.ringthealarm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PrivateAmbList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_private_amb_list);
+    }
+}
