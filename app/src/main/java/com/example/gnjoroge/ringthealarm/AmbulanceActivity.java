ca3a7af80cabc4a6e985aedeb0e7290eb1f53ddd
NjeriNjoroge <wanjorogegrace@gmail.com>
2017-10-12 16:37:35 +0300

    displaying data in a list view for now

--- app/src/main/java/com/example/gnjoroge/ringthealarm/AmbulanceActivity.java
+++ app/src/main/java/com/example/gnjoroge/ringthealarm/AmbulanceActivity.java
@@ -1,7 +1,8 @@
 package com.example.gnjoroge.ringthealarm;
 
+import android.app.FragmentManager;
 import android.content.Intent;
-import android.support.v4.app.FragmentManager;
+
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,16 +10,19 @@ import android.view.View;
 import android.widget.Button;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 
 
 public class AmbulanceActivity extends AppCompatActivity {
 
     @Bind(R.id.selectAmb) Button mSelectAmb;
+    @Bind(R.id.sendSms) Button mSendSms;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ambulance);
+        ButterKnife.bind(this);
 
         mSelectAmb.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,8 +32,6 @@ public class AmbulanceActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-
 
     }
 
--- app/src/main/java/com/example/gnjoroge/ringthealarm/PrivateAmbList.java
+++ app/src/main/java/com/example/gnjoroge/ringthealarm/PrivateAmbList.java
@@ -1,13 +1,59 @@
 package com.example.gnjoroge.ringthealarm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PrivateAmbList extends AppCompatActivity {
+
+    private String[] ambulances = new String[] {"AAR Insurance Kenya Ltd",
+    "AMREF Flying Doctors",
+    "Avenue Rescue Services",
+    "Emergency Response Services",
+    "Emergency Rescue",
+    "Rescue Med",
+    "St John Ambulance Kenya",
+    "West Ambulance Services",
+    "Kenya Red Cros Society Ambulance in Kenya",
+    "Road Safety Network Ambulance Service in Kenya",
+    "The Karen Hospital",
+    "Intensive Care Air Ambulance Ltd"};
+
+    private String[] phone = new String[] {"020-2717374, 254703063000",
+            "+254-722314239, +254-733639088",
+            "+254203745750",
+            "+254770111090",
+            "+254721775699",
+            "+254722805645",
+            "+254720736591",
+            "+254723445526",
+            "(020)3950000",
+            "2212699",
+            "+254206613000",
+            "+254-206004945"};
+
+    private ListView mlistView;
+
+//    private TextView mName;
+//    private CardView mCardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_private_amb_list);
+
+        mlistView = (ListView) findViewById(R.id.listView);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, ambulances);
+        mlistView.setAdapter(adapter);
+
+
     }
 }
