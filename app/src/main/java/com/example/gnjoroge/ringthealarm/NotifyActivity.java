75987a2fc4ba5e979239efe5294e698d6ad91a65
NjeriNjoroge <wanjorogegrace@gmail.com>
2017-10-16 16:04:33 +0300

    added a splash screen

--- app/src/main/java/com/example/gnjoroge/ringthealarm/NotifyActivity.java
+++ app/src/main/java/com/example/gnjoroge/ringthealarm/NotifyActivity.java
@@ -28,136 +28,74 @@ import butterknife.ButterKnife;
 
 public class NotifyActivity extends AppCompatActivity {
 
-    @Bind(R.id.list) ListView mListView;
-    @Bind(R.id.pickContact) Button mPickContact;
-
     ArrayList<String> contactList;
-    Cursor cursor;
-    int counter;
 
-    private ProgressDialog pDialog;
-    private Handler updateBarHandler;
+    private static final int RESULT_PICK_CONTACT = 50;
+
+    @Bind(R.id.button) Button mSelect;
+    @Bind(R.id.contactName) TextView mName;
+    @Bind(R.id.contactNum) TextView mNum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notify);
         ButterKnife.bind(this);
-
-        pDialog = new ProgressDialog(this);
-        pDialog.setMessage("Reading contacts...");
-        pDialog.setCancelable(false);
-        pDialog.show();
-
-
-
-        updateBarHandler = new Handler();
-
-        //putting read contacts on a separate thread
-
-//        mPickContact.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        getContacts();
-                    }
-
-                }).start();
-
-//            }
-
-//        });
-
-
-
-        //setting onClickListener to the list item
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                Toast.makeText(getApplicationContext(), "item clicked : \n" +contactList.get(0), Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 
-//    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-    public void getContacts() {
-        contactList = new ArrayList<String>();
-        String phoneNumber = null;
-        String email = null;
-        Uri CONTENT_URI = ContactsContract.Contacts.CONTENT_URI;
-        String _ID = ContactsContract.Contacts._ID;
-        String DISPLAY_NAME = ContactsContract.Contacts.DISPLAY_NAME;
-        String HAS_PHONE_NUMBER = ContactsContract.Contacts.HAS_PHONE_NUMBER;
-        Uri PhoneCONTENT_URI = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
-        String Phone_CONTACT_ID = ContactsContract.CommonDataKinds.Phone.CONTACT_ID;
-        String NUMBER = ContactsContract.CommonDataKinds.Phone.NUMBER;
-        Uri EmailCONTENT_URI =  ContactsContract.CommonDataKinds.Email.CONTENT_URI;
-        String EmailCONTACT_ID = ContactsContract.CommonDataKinds.Email.CONTACT_ID;
-        String DATA = ContactsContract.CommonDataKinds.Email.DATA;
-        StringBuffer output;
-        ContentResolver contentResolver = getContentResolver();
-        cursor = contentResolver.query(CONTENT_URI, null,null, null, null);
-        //iterate every contact in the phone
-        if (cursor.getCount() > 0) {
-            counter = 0;
-            while (cursor.moveToNext()) {
-                output = new StringBuffer();
-                //update the progress message
-                updateBarHandler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        pDialog.setMessage("Reading contacts : "+ counter++ +"/"+cursor.getCount());
-                    }
-                });
+    public void pickContact(View v) {
 
-                String contact_id = cursor.getString(cursor.getColumnIndex(_ID));
-                String name = cursor.getString(cursor.getColumnIndex(DISPLAY_NAME));
-                //String num = cursor.getString(cursor.getColumnIndex(NUMBER));
-                int hasPhoneNumber = Integer.parseInt(cursor.getColumnName(cursor.getColumnIndex( HAS_PHONE_NUMBER )));
 
-                if (hasPhoneNumber > 0) {
-                    output.append("\n First Name;" + name);
-                    //This is to read multiple phone numbers associated with the same contact
-                    Cursor phoneCursor = contentResolver.query(PhoneCONTENT_URI, null, Phone_CONTACT_ID + " = ?", new String[] { contact_id }, null);
-                    while (phoneCursor.moveToNext()) {
-                        phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(NUMBER));
-                        output.append("\n Phone number:" + phoneNumber);
-                    }
-                    phoneCursor.close();
-                    // Read every email id associated with the contact
-                    Cursor emailCursor = contentResolver.query(EmailCONTENT_URI,    null, EmailCONTACT_ID+ " = ?", new String[] { contact_id }, null);
-                    while (emailCursor.moveToNext()) {
-                        email = emailCursor.getString(emailCursor.getColumnIndex(DATA));
-                        output.append("\n Email:" + email);
-                    }
-                    emailCursor.close();
-                }
+        Intent contactPickerIntent = new Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+        startActivityForResult(contactPickerIntent, RESULT_PICK_CONTACT);
+    }
 
-                //adding to the array list
-                contactList.add(output.toString());
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        //check whether result is ok
+        if (resultCode == RESULT_OK) {
+            //check for the request code, startActivityForResult
+            switch (requestCode) {
+                case RESULT_PICK_CONTACT:
+                    contactPicked(data);
+                    break;
             }
 
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.list_item, R.id.text1, contactList);
-                    mListView.setAdapter(adapter);
-                }
-            });
-            //dismissing the progress bar after 500ms
-            updateBarHandler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    pDialog.cancel();
-                }
-            }, 500);
+        } else {
+            Log.e("NotifyActivity", "Failed to pick contacts");
         }
-
     }
+
+    private void contactPicked(Intent data) {
+        contactList = new ArrayList<String>();
+
+        Cursor cursor = null;
+
+        try {
+
+            String phoneNo = null;
+            String name = null;
+            // getData() method will have the Content Uri of the selected contact
+            Uri uri = data.getData();
+            //Query the content uri
+            cursor = getContentResolver().query(uri, null, null, null, null);
+            cursor.moveToFirst();
+            //column index of the phone number
+            int phoneIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+            //column index of the contact name
+            int nameIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+            phoneNo = cursor.getString(phoneIndex);
+            name = cursor.getString(nameIndex);
+            //setting values to the text views
+            mName.setText(name);
+            mNum.setText(phoneNo);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        contactList.add(mName.toString());
+    }
+
 
 
 }
